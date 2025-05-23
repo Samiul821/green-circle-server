0434f268caebaef745ab62de506b11d2d8ff7ef3
Samiul821 <smsamiul821@gmail.com>
2025-05-23 17:10:35 +0600

    delete method added

--- index.js
+++ index.js
@@ -82,6 +82,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/gardenTips/:id", async (req,res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await gardenTipsCollection.deleteOne(query);
+      res.send(result);
+    })
+
     app.post("/users", async (req, res) => {
       const userProfile = req.body;
       const result = await userCollection.insertOne(userProfile);

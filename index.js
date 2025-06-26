8d81a95d6accbff0fb45177bb48da25fb04fa91e
Samiul821 <smsamiul821@gmail.com>
2025-06-26 22:47:59 +0600

    check

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9rzwgbq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions object to set the Stable API versio
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
